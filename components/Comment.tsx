238aee9168a36ddd8e300dfc51ca4293fdaf8b88
Yuza <akhmadyuza@gmail.com>
2024-02-20 10:31:18 +0700

    Phase 1: Fixing cant find module giscus

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -1,28 +1,21 @@
-import { Giscus, GiscusProps } from 'pliny/comments/Giscus';
+import Giscus from '@giscus/react';
 
-const config = {
-    giscusConfig: {
-      repo: 'yvza/blog.comments',
-      repositoryId: 'MDEwOlJlcG9zaXRvcnkyOTI0OTU4OTQ=',
-      category: 'Q&A',
-      categoryId: 'DIC_kwDOEW8iFs4Cbs0K',
-      mapping: 'pathname',
-      reactions: '1',
-      // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
-      theme: 'light',
-      // theme when dark mode
-      darkTheme: 'light_protanopia',
-      // If the theme option above is set to 'custom`
-      // please provide a link below to your custom theme css file.
-      // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
-      lang: 'en',
-    },
-}
-
-export default function BlogComments(props: GiscusProps) {
+export default function BlogComments() {
   return (
-    <Giscus {...config.giscusConfig} {...props} />
+    <Giscus
+      id="comments"
+      repo="yvza/blog.comments"
+      repoId="MDEwOlJlcG9zaXRvcnkyOTI0OTU4OTQ="
+      category="Q&A"
+      categoryId="DIC_kwDOEW8iFs4Cbs0K"
+      mapping="pathname"
+      term="Welcome :)"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="light_tritanopia"
+      lang="en"
+      loading="lazy"
+    />
   )
 }
